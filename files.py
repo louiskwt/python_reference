bd0c28d5bfab1b98493fdac9974c274fc9acf854
Ka Wing Tsang <kawingt2@gmail.com>
2021-04-13 10:44:50 +0800

    working with files and JSON

--- files.py
+++ files.py
@@ -1,1 +1,23 @@
 # Python has functions for creating, reading, updating, and deleting files.
+# open a file
+myFile = open('myfile.txt', 'w')
+
+# get some info
+print('name: ', myFile.name)
+print('Is close: ', myFile.closed)
+print('Opening mode: ', myFile.mode)
+
+# Write to file
+myFile.write('I love python')
+myFile.write(' and JavaScript')
+myFile.close()
+
+# Append to file - w will overwrite the file
+myFile = open('myfile.txt', 'a')
+myFile.write(' I also like React')
+myFile.close()
+
+# Read from file
+myFile = open('myfile.txt', 'r+')
+text = myFile.read(100)
+print(text)
--- py_json.py
+++ py_json.py
@@ -1,1 +1,19 @@
 # JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+import json
+
+#sample json
+userJSON = '{"first_name": "John", "last_name": "Doe", "age": 30}'
+
+# Parse to dict
+user = json.loads(userJSON)
+
+print(user)
+print(user['first_name'])
+
+# Turn a dictionary into json
+car = {'make': 'Toyota', 'model': 'Puris', 'year': 2007}
+
+carJSON = json.dumps(car)
+
+# Print in JSON format with " "
+print(carJSON)
